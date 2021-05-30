0b28c1e88bc7c1c0bc3a8b48b239f17664b3f177
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-05-29 21:01:58 -0400

    Added the background image to the home page of the site.

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePic from '../../Assets/HomePic.jpeg';
 
 
 const Home = () => {
     return (
         <div>
-            
+            <img src={HomePic} alt="Website background" />
         </div>
     );
 };
