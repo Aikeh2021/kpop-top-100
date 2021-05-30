905d91c721c4a86e6428572a2fc1c3703fe77a61
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-05-30 11:34:20 -0400

    Removed some commented out code from the home.jsx file.

---
+++ client/src/Components/Buttons/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Buttons = () => {
+    return (
+        <div>
+            <button className="button is-light">Light</button>
+        </div>
+    );
+};
+
+export default Buttons;
--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import HomePic from '../../Assets/HomePic.jpeg';
 import '../Home/Home.css';
 
 
 const Home = () => {
     return (
         <div className="container container is-fullhd" id="page">
-            {/* <img src={HomePic} alt="Website background" id="bground" /> */}
             <h1 id="hp-title">Kpop Top 100</h1>
         </div>
     );
