8523c5467a0282f8111f7959c30e65840509b7d1
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-06-02 04:22:30 -0400

    Still styling the buttons on the homepage. Commiting and pushing changes prior to fixing Chrome error that's not reflecting changes in the local host.

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -5,16 +5,16 @@ import Buttons from '../../Components/Buttons/Buttons.jsx';
 
 const Home = () => {
     return (
-        <div className="container container is-fullhd" id="page">
+        <div className="container is-fullhd" id="page">
             <h1 id="hp-title">Kpop Top 100</h1>
-            <div className="row">
-                <div className="column is-half">
-                <Buttons drtn="" dspTxt="MEET THE TEAM"/>
-                </div>
+            <div className="columns">
+                <div className="column is-half hbtn-div">
+                <Buttons drtn="" dspTxt="MEET THE TEAM" className="hbtns"/>
+                </div> 
                 {/* <div className="column">
-                </div> */}
-                <div className="column is-half">
-                <Buttons drtn="" dspTxt="GET POPPING"/>
+                </div>  */}
+                <div className="column is-half hbtn-div">
+                <Buttons drtn="" dspTxt="GET POPPING" className="hbtns"/>
                 </div>
             </div>
         </div>
